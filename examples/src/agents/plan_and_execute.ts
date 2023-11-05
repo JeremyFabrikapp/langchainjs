83dcc0c3894f8afbba17c765dac11aad38ef34e0
Jeremy <jeremy@fabrikapp.fr>
2023-11-05 19:04:39 +0100

    stuff

--- examples/src/agents/plan_and_execute.ts
+++ examples/src/agents/plan_and_execute.ts
@@ -1,9 +1,23 @@
 import { Calculator } from "langchain/tools/calculator";
-import { SerpAPI } from "langchain/tools";
+import { ReadFileTool, SerpAPI, Tool, WriteFileTool } from "langchain/tools";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { PlanAndExecuteAgentExecutor } from "langchain/experimental/plan_and_execute";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { NodeFileStore } from "langchain/stores/file/node";
+const store = new NodeFileStore("node-file-store/custom_agent");
 
-const tools = [new Calculator(), new SerpAPI()];
+// const model = new OpenAI({ temperature: 0 });
+const embeddings = new OpenAIEmbeddings(
+  process.env.AZURE_OPENAI_API_KEY
+    ? { azureOpenAIApiDeploymentName: "Embeddings2" }
+    : {}
+);
+const tools = [
+  new Calculator(),
+  new SerpAPI(),
+  new ReadFileTool({ store }) as unknown as Tool,
+  new WriteFileTool({ store }) as unknown as Tool,
+];
 const model = new ChatOpenAI({
   temperature: 0,
   modelName: "gpt-3.5-turbo",
@@ -15,7 +29,35 @@ const executor = PlanAndExecuteAgentExecutor.fromLLMAndTools({
 });
 
 const result = await executor.call({
-  input: `Who is the current president of the United States? What is their current age raised to the second power?`,
+  // input: `You need to create a nextJS project. NodeJS is installed on your computer. You have access to the following tools: search, calculator, read_file, write_file, web-browser.`,
+  input: `
+Find the current president of the USA and write an a txt file of 200 words and store it in a file called result.txt. To store the result in a variable, you can use the write_file tool with props "file_path" that is a name of file and "text" with your reply. 
+You MUST USE EXACTLY the following json format to store the result as the following Examples: 
+
+Human : "I need to use the write_file tool to write the name of the president to a file"
+Assistant : 
+"{
+  "action": "write_file",
+  "action_input": {
+      "file_path": "result.txt",
+      "text": "<Your answer here>"
+  }}.
+"
+
+Human : "I need to read a file, how can I do that ?"
+Assistant : 
+"{
+  "action": "read_file",
+  "action_input": {
+      "file_path": "result.txt",
+  }}.
+"
+// End of examples
+
+Now you can start !
+`,
 });
 
 console.log({ result });
+// Human : "I need to search for the current president of the USA"
+// Assistant :
